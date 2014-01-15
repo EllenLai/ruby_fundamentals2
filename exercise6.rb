484dd5124213f1638fca94e9bad57e5c88996d4a
EllenLai <miss.ellenlai@gmail.com>
2014-01-15 15:02:45 -0500

    adding rice

--- exercise6.rb
+++ exercise6.rb
@@ -1,15 +1,27 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 
+
 def grocerydisplay(grocery_list)
 	grocery_list.each do |item|
 		puts "* #{item}"
 
-end
+	end
 
 end
 
-grocerydisplay(grocery_list)
+def add_grocery(grocery_list, item)
+	grocery_list.push(item)
+	 grocerydisplay(grocery_list)
+
+	end
+
+
+
+add_grocery(grocery_list, "rice")
+
+
+
 
 
 
