b815b9b27d16fc6941910f0aa11d05ff418bb1ae
EllenLai <miss.ellenlai@gmail.com>
2014-01-15 12:06:23 -0500

    fixed code to list properly

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,16 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+
+def grocerydisplay(grocery_list)
 	grocery_list.each do |item|
 		puts "* #{item}"
+
 end
+
+end
+
+grocerydisplay(grocery_list)
+
+
+
 
