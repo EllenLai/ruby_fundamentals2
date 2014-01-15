996c30f43418f663436f1b0693b61db6f6ab1801
EllenLai <miss.ellenlai@gmail.com>
2014-01-15 16:58:13 -0500

    increasing class number by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -10,4 +10,10 @@ students.each do |key, value|
 	puts "#{key}: #{value}"
 end
 
+puts "New Cohort increase by 5%"
+
+students.each do |key, value|
+	value = (value * 0.05 + value).to_i
+	puts "#{key}: #{value}"
+end
 
