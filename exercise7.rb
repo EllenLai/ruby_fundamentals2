5f9d25057897a845df2b3c8a321c1d356aed173c
EllenLai <miss.ellenlai@gmail.com>
2014-01-15 21:47:11 -0500

    bonus question

--- exercise7.rb
+++ exercise7.rb
@@ -10,19 +10,45 @@ students.each do |key, value|
 	puts "#{key}: #{value}"
 end
 
+puts "\n"
 
 def increase(students)
 	puts "Cohort increase by 5%"
-		students.each do |key, value|
-			value = (value * 0.05 + value).to_i
-				puts "#{key}: #{value}"
+	students.each do |key, value|
+		new_value = (value * 1.05).to_i
+		students[key] = new_value
+		puts "#{key}: #{new_value}"
 	end
 end
 
 def delete_cohort(students)
 	students.delete(:cohort2)
-	increase(students)
+
 end
 
 increase(students)
 delete_cohort(students)
+
+puts "\n"
+
+students.each do |key, value|
+	puts "#{key}: #{value}"
+end
+puts "\n"
+
+
+def calctotal(students)
+        total=0
+        students.each{
+                |x,y| total+=y 
+        }
+        puts "The total number of students is #{total}."
+end
+
+calctotal(students)
+
+
+
+
+
+
